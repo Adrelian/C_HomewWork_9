097e1e6a91bc9b8e7f77e739bf329e585896807d
Anatolii <anatolii.polovnikov@gmail.com>
2022-12-12 23:13:12 +0500

    задача 2

--- Program.cs
+++ Program.cs
@@ -4,25 +4,48 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-void allNumber (int finish)
-{
-    int start = 1;
-    if (finish >= start)
-    {
-        allNumber(finish - 1);
-        Console.Write(finish + " ");
-    }
-}
+// void allNumber (int finish)
+// {
+//     int start = 1;
+//     if (finish >= start)
+//     {
+//         allNumber(finish - 1);
+//         Console.Write(finish + " ");
+//     }
+// }
 
-Console.Write("Введите первое число последовательности ");
-int start = Convert.ToInt32(Console.ReadLine());
-allNumber(start);
+// Console.Write("Введите первое число последовательности ");
+// int start = Convert.ToInt32(Console.ReadLine());
+// allNumber(start);
 
 // Задача 66: Задайте значения M и N. Напишите программу,
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+int sumAllNumber(int start, int finish)
+{
+    if(start==finish) return start;
+
+   if (start < finish)
+   {
+        start = start + sumAllNumber(start + 1, finish);
+        return start;
+   }
+   else
+   {
+        start = start + sumAllNumber(start - 1, finish);
+        return start;
+   }
+}
+
+Console.Write("Введите первое число ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число ");
+int finish = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(sumAllNumber(start, finish));
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
 // Даны два неотрицательных числа m и n.
